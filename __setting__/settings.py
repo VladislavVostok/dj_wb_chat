e8e48c827425b31f3239404bf421f54db0b31748
vladislav_vostok <vladislav.sage@yandex.ru>
2024-04-24 16:31:39 +0300

    added allowed hosts

--- __setting__/settings.py
+++ __setting__/settings.py
@@ -1,22 +1,12 @@
-
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-$!(z0fy)6hkx@q9q7u!zvu%3k+3tw=ogfdoptv%3ic&=j!x+*5'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['v-east.ru', 'www.v-east.ru']
-
-
-# Application definition
+ALLOWED_HOSTS = ['v-east.ru', 'www.v-east.ru', '127.0.0.1']
 
 INSTALLED_APPS = [
     'daphne',
@@ -54,7 +44,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = '__setting__.urls'
 
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/chats/"
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 TEMPLATES = [
@@ -73,18 +63,12 @@ TEMPLATES = [
     },
 ]
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -101,8 +85,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
 
 LANGUAGE_CODE = 'ru-ru'
 TIME_ZONE = 'Europe/Moscow'
@@ -110,13 +92,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
 STATIC_ROOT = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
